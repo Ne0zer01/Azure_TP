007f67a7172983f260bb4eb1936c5f41e4c51093
Ne0zer01 <djamil.khennouchi@efrei.net>
2025-09-13 18:07:51 +0200

    Modification du Git

--- TP2/TerraForm/keyvault.tf
+++ TP2/TerraForm/keyvault.tf
@@ -1,5 +1,8 @@
+# Récupère les infos de ton compte Azure
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_key_vault" "meow_vault" {
-  name                       = "vaulttest"
+  name                       = "vaulttestdjamil"
   location                   = azurerm_resource_group.main.location
   resource_group_name        = azurerm_resource_group.main.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
@@ -33,7 +36,7 @@ resource "random_password" "meow_secret" {
 
 # au hasard, pour créer un Secret dans une Azure Key Vault :)
 resource "azurerm_key_vault_secret" "meow_secret" {
-  name         = "vaultpassword"
+  name         = "vaultpassworddjamil"
   value        = random_password.meow_secret.result
   key_vault_id = azurerm_key_vault.meow_vault.id
 }
